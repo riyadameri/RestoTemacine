cb00fd02ffa4dcbb179e74c4a58ca5a8fa700ad7
Riyad Ameri <riyadammmeri@gmail.com>
2025-05-17 22:07:55 +0200

    sd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
             const foodItems = document.getElementById('foodItems');
             const cartItems = document.getElementById('cartItems');
             const cartCount = document.getElementById('cartCount');
